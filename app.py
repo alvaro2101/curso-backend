3a64f55e98a796813f8daa15c444ffa8ed10f22c
Bloodman <alvaroroman@gmail.com>
2026-01-09 14:28:18 -0500

    Actualizacion: Ahora el script saluda e interactua

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("¡Hola Backend! Soy Bloodman y ya tengo Python")
+nombre = input("¿Cual es tu nombre? ")
+edad = input("¿Cuantos años tienes? ")
+
+edad_numero = int(edad)
+edad_en_10_anos = edad_numero + 10
+
+print("---------------------------")
+print("Hola" + nombre + ", gusto en conocerte prro.")
+print("En 10 años tendras" + str(edad_en_10_anos) + " años.")
+print("---------------------------")
